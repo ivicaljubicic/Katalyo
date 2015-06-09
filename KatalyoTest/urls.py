b760350d5609d1a9efa3da24bd086f7f872c97ef
ivicaljubicic <ivicaljubicic@yahoo.com>
2015-06-09 18:09:31 +0200

    Including AngularJS
    Including AngularJS

--- KatalyoTest/urls.py
+++ KatalyoTest/urls.py
@@ -17,9 +17,20 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from resources.views import resourcedef
 
+from rest_framework import routers
+from resources import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^resource-def/', resourcedef,name='resourcedef'),
-    
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(router.urls)),
 ]
--- resources/views.py
+++ resources/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from resources.serializers import UserSerializer, GroupSerializer
+
 
 # Create your views here.
 def resourcedef(request):
     context = RequestContext(request)
     return render_to_response('resource-def.html', {}, context)
+
+#Django REST API classes
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
